78a80b339f13c1211a95a42e7087cfb9678ac563
Gamma <gamma2011fr@gmail.com>
2021-09-24 15:49:32 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,74 @@
+
+/***********************************************************************************************************************
+ * COMPANY : 
+ *
+ * PROJECT  : 
+ *
+ ***********************************************************************************************************************
+ * !File            : 
+ *
+ * !Component       : 
+ * !Description     : 
+ *
+ * !Module          : 
+ * !Description     :
+ *
+ * !Author          :                    !Date: 24.09.2021
+ * !Coding Language : C
+ *
+ **********************************************************************************************************************/
+
+/**********************************************************************************************************************/
+/* INCLUDE FILES                                                                                                      */
+/**********************************************************************************************************************/
+#include"plus_ou_moins.h"
+
+/**********************************************************************************************************************/
+/* MAIN FUNCTION                                                                                                      */
+/**********************************************************************************************************************/
+int main(void)
+{
+    srand(time(NULL)); // Initialisation du générateur de nombres
+
+    int Choice;
+    int Min = MIN_DEFAULT;
+    int Max = MAX_DEFAULT;
+    int NbLives = NB_VIES_DEFAULT;
+    int Game = TRUE;
+
+    printf("\n]==================================================[\n");
+    printf("       BIENVENUE DANS LE JEU \"plus ou moins\" !");
+    printf("\n]==================================================[\n");
+
+    while(Game)
+    {
+        printf("\n]===== MENU :\n");
+        printf("\n  - 1) Afficher les regles.");
+        printf("\n  - 2) Modifier les parametres.");
+        printf("\n  - 3) Commencer une partie.");
+        printf("\n  - 4) Quitter.\n");
+        printf("\n  (entrez le numero correspondant) : ");
+        scanf_s("%d", &Choice);
+
+        switch(Choice)
+        {
+            case 1:
+                printf("\n]===== REGLES DU JEU :\n");
+                break;
+            case 2:
+                printf("\n]===== PARAMETRES :\n");
+                changeOptions(&Min, &Max, &NbLives);
+                break;
+            case 3:
+                printf("\n]===== NOUVELLE PARTIE :\n");
+                startGame(&Min, &Max, &NbLives);
+                break;
+            default:
+                printf("\nAu revoir :)\n");
+                Game = FALSE;
+                break;
+        }
+    }
+
+    return 0;
+}
